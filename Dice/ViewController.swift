88ab706ecef4dae47b36c9d23deb5dd8e028d189
Tommy <tommyzhangchina@gmail.com>
2020-07-27 23:01:40 +0800

    0.1 Basic Function
    Support for two dice

---
+++ Dice/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by ZhangTommy on 27/7/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dice: UIButton!
+    
+    @IBAction func cal(_ sender: Any) {
+        let d1 = Int.random(in: 1..<6)
+        let d2 = Int.random(in: 0..<6)
+        let total = d1+d2
+        dice.setTitle(String(total), for: .normal)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
